a763a4d77fb369cdfa7310e268a17687d0cbfaf7
Andreas Haller <andreashaller@gmail.com>
2010-09-16 20:47:16 +0200

    Fix generator - renamed it so rails can find it

--- lib/generators/shop_bunny/install_generator.rb
+++ lib/generators/shop_bunny/install_generator.rb
@@ -1,7 +1,7 @@
 module ShopBunny
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path("../../../../", __FILE__)
-    
+
     def copy_files
      puts Dir['.']
      directory 'db/migrate'
