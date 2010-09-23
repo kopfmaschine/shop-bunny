78dac7b2f125595fba61589e8af1e66664852d22
Jan Steinmetz <jan@kopfmaschine.com>
2010-09-24 00:33:22 +0200

    included the controller to be required in the libfile that the gem should actually work

--- lib/shop_bunny.rb
+++ lib/shop_bunny.rb
@@ -3,6 +3,7 @@ module ShopBunny
   require 'shop_bunny/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
   require 'shop_bunny/shipping_cost_calculator'
   require 'shop_bunny/cart_module'
+  require 'shop_bunny/cart_controller_module'
   
   mattr_accessor :item_model_class_name
   @@item_model_class_name = 'Item'
