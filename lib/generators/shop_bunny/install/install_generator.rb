26fa82e50a31f369abf0879c5cb535427462cc04
Jan Steinmetz <jan@kopfmaschine.com>
2012-03-28 12:22:26 +0200

    fix generator

---
+++ lib/generators/shop_bunny/install/install_generator.rb
@@ -0,0 +1,30 @@
+require 'rails/generators'
+module ShopBunny  
+  class InstallGenerator < Rails::Generators::Base
+    include Rails::Generators::Migration
+    class_option :model, :type => :boolean, :default => true, :desc => "copy model templatefile."
+
+    def copy_files
+      puts Dir['.']
+      copy_file "#{self.class.source_root}/initializer.rb", 'config/initializers/shop_bunny.rb'
+      copy_file "#{self.class.source_root}/shopbunny_model_template.rb", 'app/models/cart.rb' if options.model?
+    end
+
+    def self.source_root
+      @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+    end
+
+    def create_migrations
+      puts "BAA"
+      p self.class.source_root
+      Dir["#{self.class.source_root}/../../../../../db/migrate/*.rb"].sort.each do |filepath|
+        p filepath
+        name = File.basename(filepath)
+        copy_file filepath, "db/migrate/#{name}"
+      end
+    end
+
+  end
+end
+
+# TODO Write a generator test
